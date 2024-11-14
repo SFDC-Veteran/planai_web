1a0fc9f463b21cf331985aa7d363fe5e038cbd47
tjduddn8511 <130065523+tjduddn8511@users.noreply.github.com>
2024-11-14 11:02:57 +0900

    ai terminal success

--- src/components/common/bottombar/index.tsx
+++ src/components/common/bottombar/index.tsx
@@ -1,3 +1,236 @@
+// import * as S from "./style";
+// import Export from "src/assets/images/common/bottombar/export.png";
+// import Share from "src/assets/images/common/bottombar/share.png";
+// import Plus from "src/assets/images/common/bottombar/plus.png";
+// import UseBottomBar from "src/hooks/common/bottombar/useBottomBar";
+// import UseWrite from "src/hooks/write/useWrite";
+// import Save from "src/assets/images/common/bottombar/save.png";
+// import Select from "src/assets/images/common/bottombar/selectpage.png";
+// import Page from "src/assets/images/common/bottombar/page.png";
+// import planaiAxios from "src/libs/axios/customAxios";
+// import Chat from "src/assets/images/common/bottombar/chat.png";
+// import AI from "src/assets/images/common/bottombar/ai.png";
+// import Back from "src/assets/images/common/bottombar/back.png";
+// import { PageData } from "src/types/write/page.type";
+// import React, { SetStateAction, useState } from "react";
+// import bottomStore from "src/store/common/bottom.store";
+// import Ai from "./components/ai";
+// import User from "./components/user";
+// import UseAi from "src/hooks/ai/useAi";
+
+// interface Props {
+//   data: PageData;
+//   setData: React.Dispatch<SetStateAction<PageData>>;
+// }
+
+// const BottomBar = ({ data, setData }: Props) => {
+//   const { PostPage, now } = UseBottomBar();
+//   const { PatchPage, setOnClick, onclick } = UseWrite();
+//   const {
+//     AcademicButton,
+//     RedditButton,
+//     WebButton,
+//     WolframButton,
+//     WriteButton,
+//     YoutubeButton,
+//     academic,
+//     reddit,
+//     web,
+//     wolfram,
+//     write,
+//     youtube,
+//     setText,
+//     text,
+//     chatHistory,
+//   } = UseAi();
+//   const setPlanId = bottomStore((state) => state.setPlanId);
+//   console.log("academic", academic);
+//   console.log("reddit", reddit);
+//   console.log("web", web);
+//   console.log("wolfram", wolfram);
+//   console.log("write", write);
+//   console.log("youtube", youtube);
+//   const [selectedAi, setSelectedAi] = useState<string>("youtube");
+//   const [color, setColor] = useState<boolean>(false);
+
+//   const handleClick = () => {
+//     if (!text) return; // 텍스트가 비어 있으면 실행하지 않음
+
+//     // 선택된 AI 종류에 따라 다른 함수 호출
+//     switch (selectedAi) {
+//       case "youtube":
+//         YoutubeButton();
+//         break;
+//       case "write":
+//         WriteButton();
+//         break;
+//       case "wolfram":
+//         WolframButton();
+//         break;
+//       case "web":
+//         WebButton();
+//         break;
+//       case "reddit":
+//         RedditButton();
+//         break;
+//       case "academic":
+//         AcademicButton();
+//         break;
+//       default:
+//         YoutubeButton(); // 기본적으로 YouTube 요청
+//     }
+//   };
+//   const [ai, setAi] = useState<boolean>(false);
+
+//   const GetPage = async (id: number) => {
+//     await planaiAxios
+//       .get(`/plan/single/${id}`)
+//       .then((res) => {
+//         setData(res.data.data);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       });
+//   };
+
+//   return (
+//     <>
+//       {ai === false ? (
+//         <S.BarWrapper>
+//           <S.PlusButton onClick={PostPage}>
+//             <img src={Plus} style={{ width: 30, height: 30 }} alt="" />
+//           </S.PlusButton>
+//           {Array.isArray(now) &&
+//             now.map((item, idx) => (
+//               <S.PlanButton
+//                 key={idx}
+//                 onClick={() => {
+//                   setOnClick(item.id);
+//                   GetPage(item.id);
+//                   setPlanId(item.id);
+//                 }}
+//               >
+//                 {onclick === item.id ? (
+//                   <img src={Select} style={{ width: 50, height: 50 }} alt="" />
+//                 ) : (
+//                   <img src={Page} style={{ width: 50, height: 50 }} alt="" />
+//                 )}
+//               </S.PlanButton>
+//             ))}
+
+//           <S.BarMainWrapper>
+//             <S.IconButton onClick={PatchPage}>
+//               <img src={Save} style={{ width: 30, height: 30 }} alt="" />
+//             </S.IconButton>
+//             <S.IconButton>
+//               <img src={Export} style={{ width: 30, height: 30 }} alt="" />
+//             </S.IconButton>
+//             <S.IconButton>
+//               <img src={Share} style={{ width: 30, height: 30 }} alt="" />
+//             </S.IconButton>
+//             <S.IconButton style={{ background: "#353866" }} onClick={() => setAi(true)}>
+//               <img src={AI} style={{ width: 30, height: 30 }} alt="" />
+//             </S.IconButton>
+//           </S.BarMainWrapper>
+//         </S.BarWrapper>
+//       ) : (
+//         <S.AiBarWrapper>
+//           <S.AiMainWrapper>
+//             <S.BackButtonWrapper>
+//               <img src={Back} style={{ width: 15, height: 25 }} alt="" onClick={() => setAi(false)} />
+//             </S.BackButtonWrapper>
+//             <S.AiChatWrapper>
+//               {chatHistory.map((item, idx) => (
+//                 <div key={idx}>
+//                   <User text={item.user} />
+//                   {item.ai && (
+//                     <Ai
+//                       youtube={item.ai}
+//                       academic={item.ai}
+//                       reddit={item.ai}
+//                       web={item.ai}
+//                       wolfram={item.ai}
+//                       write={item.ai}
+//                     />
+//                   )}
+//                 </div>
+//               ))}
+//             </S.AiChatWrapper>
+//             <S.AiTextModeWrapper>
+//               <S.AiTextModeMainWrapper
+//                 style={{ paddingRight: 10, borderRight: "1px solid #fff" }}
+//                 onClick={() => {
+//                   setSelectedAi("youtube");
+//                   setColor(!color);
+//                 }}
+//                 clicked={color}
+//               >
+//                 유튜브
+//               </S.AiTextModeMainWrapper>
+//               <S.AiTextModeMainWrapper
+//                 style={{ paddingLeft: 10, paddingRight: 10, borderRight: "1px solid #fff" }}
+//                 onClick={() => {
+//                   setSelectedAi("write");
+//                   setColor(!color);
+//                 }}
+//                 clicked={color}
+//               >
+//                 쓰기 보조
+//               </S.AiTextModeMainWrapper>
+//               <S.AiTextModeMainWrapper
+//                 style={{ paddingLeft: 10, paddingRight: 10, borderRight: "1px solid #fff" }}
+//                 onClick={() => {
+//                   setSelectedAi("wolfram");
+//                   setColor(!color);
+//                 }}
+//                 clicked={color}
+//               >
+//                 wolfram Alpha
+//               </S.AiTextModeMainWrapper>
+//               <S.AiTextModeMainWrapper
+//                 style={{ paddingLeft: 10, paddingRight: 10, borderRight: "1px solid #fff" }}
+//                 onClick={() => {
+//                   setSelectedAi("web");
+//                   setColor(!color);
+//                 }}
+//                 clicked={color}
+//               >
+//                 전체
+//               </S.AiTextModeMainWrapper>
+//               <S.AiTextModeMainWrapper
+//                 style={{ paddingLeft: 10, paddingRight: 10, borderRight: "1px solid #fff" }}
+//                 onClick={() => {
+//                   setSelectedAi("reddit");
+//                   setColor(!color);
+//                 }}
+//                 clicked={color}
+//               >
+//                 레딧
+//               </S.AiTextModeMainWrapper>
+//               <S.AiTextModeMainWrapper
+//                 style={{ paddingLeft: 10 }}
+//                 onClick={() => {
+//                   setSelectedAi("academic");
+//                   setColor(!color);
+//                 }}
+//                 clicked={color}
+//               >
+//                 학술검색
+//               </S.AiTextModeMainWrapper>
+//             </S.AiTextModeWrapper>
+//             <S.AiTextWrapper>
+//               <S.AiTextInput placeholder="명령어를 입력해주세요" onChange={(e) => setText(e.target.value)} />
+//               <img src={Chat} style={{ height: 30, width: 30 }} alt="" onClick={handleClick} />
+//             </S.AiTextWrapper>
+//           </S.AiMainWrapper>
+//         </S.AiBarWrapper>
+//       )}
+//     </>
+//   );
+// };
+
+// export default BottomBar;
+
 import * as S from "./style";
 import Export from "src/assets/images/common/bottombar/export.png";
 import Share from "src/assets/images/common/bottombar/share.png";
@@ -8,9 +241,15 @@ import Save from "src/assets/images/common/bottombar/save.png";
 import Select from "src/assets/images/common/bottombar/selectpage.png";
 import Page from "src/assets/images/common/bottombar/page.png";
 import planaiAxios from "src/libs/axios/customAxios";
+import Chat from "src/assets/images/common/bottombar/chat.png";
+import AI from "src/assets/images/common/bottombar/ai.png";
+import Back from "src/assets/images/common/bottombar/back.png";
 import { PageData } from "src/types/write/page.type";
 import React, { SetStateAction, useState } from "react";
 import bottomStore from "src/store/common/bottom.store";
+import Ai from "./components/ai";
+import User from "./components/user";
+import UseAi from "src/hooks/ai/useAi";
 
 interface Props {
   data: PageData;
@@ -20,7 +259,57 @@ interface Props {
 const BottomBar = ({ data, setData }: Props) => {
   const { PostPage, now } = UseBottomBar();
   const { PatchPage, setOnClick, onclick } = UseWrite();
+  const {
+    AcademicButton,
+    RedditButton,
+    WebButton,
+    WolframButton,
+    WriteButton,
+    YoutubeButton,
+    academic,
+    reddit,
+    web,
+    wolfram,
+    write,
+    youtube,
+    setText,
+    text,
+    chatHistory,
+  } = UseAi();
   const setPlanId = bottomStore((state) => state.setPlanId);
+  const [selectedAi, setSelectedAi] = useState<string>("youtube");
+  const [ai, setAi] = useState<boolean>(false);
+
+  // 각 버튼 색상 상태 관리: 0은 기본 색, 1은 클릭된 색
+  const [selectedButton, setSelectedButton] = useState<number>(0);
+
+  const handleClick = () => {
+    if (!text) return; // 텍스트가 비어 있으면 실행하지 않음
+
+    // 선택된 AI 종류에 따라 다른 함수 호출
+    switch (selectedAi) {
+      case "youtube":
+        YoutubeButton();
+        break;
+      case "write":
+        WriteButton();
+        break;
+      case "wolfram":
+        WolframButton();
+        break;
+      case "web":
+        WebButton();
+        break;
+      case "reddit":
+        RedditButton();
+        break;
+      case "academic":
+        AcademicButton();
+        break;
+      default:
+        YoutubeButton(); // 기본적으로 YouTube 요청
+    }
+  };
 
   const GetPage = async (id: number) => {
     await planaiAxios
@@ -33,41 +322,126 @@ const BottomBar = ({ data, setData }: Props) => {
       });
   };
 
-  return (
-    <S.BarWrapper>
-      <S.PlusButton onClick={PostPage}>
-        <img src={Plus} style={{ width: 30, height: 30 }} alt="" />
-      </S.PlusButton>
-      {Array.isArray(now) &&
-        now.map((item, idx) => (
-          <S.PlanButton
-            key={idx}
-            onClick={() => {
-              setOnClick(item.id);
-              GetPage(item.id);
-              setPlanId(item.id);
-            }}
-          >
-            {onclick === item.id ? (
-              <img src={Select} style={{ width: 50, height: 50 }} alt="" />
-            ) : (
-              <img src={Page} style={{ width: 50, height: 50 }} alt="" />
-            )}
-          </S.PlanButton>
-        ))}
+  const handleAiButtonClick = (aiType: string, buttonIndex: number) => {
+    setSelectedAi(aiType);
+    setSelectedButton(buttonIndex); // 버튼 색상 상태 변경
+  };
 
-      <S.BarMainWrapper>
-        <S.IconButton onClick={PatchPage}>
-          <img src={Save} style={{ width: 30, height: 30 }} alt="" />
-        </S.IconButton>
-        <S.IconButton>
-          <img src={Export} style={{ width: 30, height: 30 }} alt="" />
-        </S.IconButton>
-        <S.IconButton>
-          <img src={Share} style={{ width: 30, height: 30 }} alt="" />
-        </S.IconButton>
-      </S.BarMainWrapper>
-    </S.BarWrapper>
+  return (
+    <>
+      {ai === false ? (
+        <S.BarWrapper>
+          <S.PlusButton onClick={PostPage}>
+            <img src={Plus} style={{ width: 30, height: 30 }} alt="" />
+          </S.PlusButton>
+          {Array.isArray(now) &&
+            now.map((item, idx) => (
+              <S.PlanButton
+                key={idx}
+                onClick={() => {
+                  setOnClick(item.id);
+                  GetPage(item.id);
+                  setPlanId(item.id);
+                }}
+              >
+                {onclick === item.id ? (
+                  <img src={Select} style={{ width: 50, height: 50 }} alt="" />
+                ) : (
+                  <img src={Page} style={{ width: 50, height: 50 }} alt="" />
+                )}
+              </S.PlanButton>
+            ))}
+
+          <S.BarMainWrapper>
+            <S.IconButton onClick={PatchPage}>
+              <img src={Save} style={{ width: 30, height: 30 }} alt="" />
+            </S.IconButton>
+            <S.IconButton>
+              <img src={Export} style={{ width: 30, height: 30 }} alt="" />
+            </S.IconButton>
+            <S.IconButton>
+              <img src={Share} style={{ width: 30, height: 30 }} alt="" />
+            </S.IconButton>
+            <S.IconButton style={{ background: "#353866" }} onClick={() => setAi(true)}>
+              <img src={AI} style={{ width: 30, height: 30 }} alt="" />
+            </S.IconButton>
+          </S.BarMainWrapper>
+        </S.BarWrapper>
+      ) : (
+        <S.AiBarWrapper>
+          <S.AiMainWrapper>
+            <S.BackButtonWrapper>
+              <img src={Back} style={{ width: 15, height: 25 }} alt="" onClick={() => setAi(false)} />
+            </S.BackButtonWrapper>
+            <S.AiChatWrapper>
+              {chatHistory.map((item, idx) => (
+                <div key={idx}>
+                  <User text={item.user} />
+                  {item.ai && (
+                    <Ai
+                      youtube={item.ai}
+                      academic={item.ai}
+                      reddit={item.ai}
+                      web={item.ai}
+                      wolfram={item.ai}
+                      write={item.ai}
+                    />
+                  )}
+                </div>
+              ))}
+            </S.AiChatWrapper>
+            <S.AiTextModeWrapper>
+              <S.AiTextModeMainWrapper
+                style={{ paddingRight: 10, borderRight: "1px solid #fff" }}
+                onClick={() => handleAiButtonClick("youtube", 1)} // 버튼 클릭 시 색상 변경
+                clicked={selectedButton === 1}
+              >
+                유튜브
+              </S.AiTextModeMainWrapper>
+              <S.AiTextModeMainWrapper
+                style={{ paddingLeft: 10, paddingRight: 10, borderRight: "1px solid #fff" }}
+                onClick={() => handleAiButtonClick("write", 2)} // 버튼 클릭 시 색상 변경
+                clicked={selectedButton === 2}
+              >
+                쓰기 보조
+              </S.AiTextModeMainWrapper>
+              <S.AiTextModeMainWrapper
+                style={{ paddingLeft: 10, paddingRight: 10, borderRight: "1px solid #fff" }}
+                onClick={() => handleAiButtonClick("wolfram", 3)} // 버튼 클릭 시 색상 변경
+                clicked={selectedButton === 3}
+              >
+                wolfram Alpha
+              </S.AiTextModeMainWrapper>
+              <S.AiTextModeMainWrapper
+                style={{ paddingLeft: 10, paddingRight: 10, borderRight: "1px solid #fff" }}
+                onClick={() => handleAiButtonClick("web", 4)} // 버튼 클릭 시 색상 변경
+                clicked={selectedButton === 4}
+              >
+                전체
+              </S.AiTextModeMainWrapper>
+              <S.AiTextModeMainWrapper
+                style={{ paddingLeft: 10, paddingRight: 10, borderRight: "1px solid #fff" }}
+                onClick={() => handleAiButtonClick("reddit", 5)} // 버튼 클릭 시 색상 변경
+                clicked={selectedButton === 5}
+              >
+                레딧
+              </S.AiTextModeMainWrapper>
+              <S.AiTextModeMainWrapper
+                style={{ paddingLeft: 10 }}
+                onClick={() => handleAiButtonClick("academic", 6)} // 버튼 클릭 시 색상 변경
+                clicked={selectedButton === 6}
+              >
+                학술검색
+              </S.AiTextModeMainWrapper>
+            </S.AiTextModeWrapper>
+            <S.AiTextWrapper>
+              <S.AiTextInput placeholder="명령어를 입력해주세요" onChange={(e) => setText(e.target.value)} />
+              <img src={Chat} style={{ height: 30, width: 30 }} alt="" onClick={handleClick} />
+            </S.AiTextWrapper>
+          </S.AiMainWrapper>
+        </S.AiBarWrapper>
+      )}
+    </>
   );
 };
 
--- src/components/common/bottombar/style.ts
+++ src/components/common/bottombar/style.ts
@@ -12,7 +12,7 @@ export const BarWrapper = styled.div`
 `;
 
 export const BarMainWrapper = styled.div`
-  width: 250px;
+  width: 350px;
   height: 60px;
   display: flex;
 
@@ -62,4 +62,110 @@ export const PlanButton = styled.div`
   justify-content: center;
   border-radius: 10px;
   margin-right: 30px;
+`;
+
+export const AiBarWrapper = styled.div`
+  display: inline-flex;
+  position: absolute;
+  box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.16);
+  border-radius: 14px;
+  background-color: #fff;
+  padding: 10px 10px 10px 10px;
+  bottom: 20px;
+  background-color: #353866;
+`;
+
+export const AiMainWrapper = styled.div`
+  width: 715px;
+  height: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const BackButtonWrapper = styled.div`
+  width: 100%;
+  height: 25px;
+  display: flex;
+`;
+
+export const AiChatWrapper = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+`;
+
+export const AiChatSpaceWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+`;
+
+export const AiTextWrapper = styled.div`
+  width: calc(100%-60px);
+  height: 60px;
+  border-radius: 14px;
+  padding-left: 30px;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const AiTextInput = styled.input`
+  width: 85%;
+  height: 60px;
+  font-size: 20px;
+  border: none;
+  outline: none;
+`;
+
+export const AiTextModeWrapper = styled.div`
+  position: absolute;
+  display: inline-flex;
+  width: 400px;
+  height: 40px;
+  padding: 0px 0px 0px 20px;
+  align-items: center;
+  border-radius: 4px;
+  bottom: 80px;
+  right: 20px;
+  background-color: #e75ba6;
+`;
+
+export const AiTextModeMainWrapper = styled.div<{ clicked: boolean }>`
+  width: auto;
+  height: auto;
+  font-size: 14px;
+  color: ${(props) => (props.clicked ? "#000" : "#fff")}; /* 클릭된 버튼은 흰색 글씨, 아니면 검은색 */
+`;
+
+export const AiChatTextWrapper = styled.div`
+  width: auto;
+  height: auto;
+  padding: 10px;
+  display: flex;
+  background-color: #fff;
+  border-radius: 5px;
+  flex-direction: column;
+`;
+
+export const AiChatTextMainWrapper = styled.div`
+  font-size: 14px;
+
+  max-width: 200px;
+
+  white-space: pre-wrap; /* 줄넘김 설정 */
+  overflow-wrap: break-word; /* 단어가 길 때 줄넘김 */
+  word-break: break-word; /* 단어가 너무 길 때 줄넘김 */
+`;
+
+export const AiPeachWrapper = styled.div`
+  width: auto;
+  height: auto;
+  display: flex;
+  align-items: center;
 `;
