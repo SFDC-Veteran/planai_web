b2b9f134c397128b135d3d3a3ef7091aef7a7f5b
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2024-11-14 17:19:45 +0900

    Update login.repositoryImpl.ts

--- src/repository/auth/login/login.repositoryImpl.ts
+++ src/repository/auth/login/login.repositoryImpl.ts
@@ -1,22 +1,26 @@
-import axios from "axios";
-import { LoginRepository } from "./login.repository";
-import { Login, NewAccessTokenResponse, TokenResponse } from "src/types/auth/auth.type";
+import axios from 'axios';
+import { LoginRepository } from './login.repository';
+import { Login, NewAccessTokenResponse, TokenResponse } from 'src/types/auth/auth.type';
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
+  withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
 class AuthRepositoryImpl implements LoginRepository {
   public async postLogin(loginParams: Login): Promise<TokenResponse> {
-    const { data } = await axiosInstance.post("/auth/sign-in", loginParams);
+    const { data } = await axiosInstance.post('/auth/sign-in', loginParams);
     return data;
   }
 
   public async refreshAccessToken(refreshToken: { refreshToken: string }): Promise<NewAccessTokenResponse> {
-    const { data } = await axios.post<NewAccessTokenResponse>(`${process.env.REACT_APP_SERVER_URL}/auth/refresh`, refreshToken);
+    const { data } = await axios.post<NewAccessTokenResponse>(
+      `${process.env.REACT_APP_SERVER_URL}/auth/refresh`,
+      refreshToken
+    );
     return data;
   }
 }
