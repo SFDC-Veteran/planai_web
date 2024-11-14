403b31f62a6e5e077847e35853af6b8b854cc666
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2024-11-14 17:57:18 +0900

    feat :: 사이드바 비율 조정 && url 삭제

--- src/components/common/sidebar/index.tsx
+++ src/components/common/sidebar/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as S from './style';
-import { News, NewsClicked, InButton } from 'src/assets/images/common/sidebar';
+import { News, NewsClicked, InButton, AI, AIClicked } from 'src/assets/images/common/sidebar';
 import useSidebar from 'src/hooks/common/sidebar/useSIdebar';
 import useScrapedNewsStore from 'src/store/Home/news.store';
 import URL from 'src/assets/images/newspaper.svg';
@@ -19,11 +19,11 @@ const Sidebar = () => {
 
   return (
     <S.SidebarWrap isvisible={isVisible}>
-      <S.SidebarItem iscliicked={item === '뉴스'} onClick={() => setItem('뉴스')}>
-        <img src={item !== '뉴스' ? News : NewsClicked} alt="" />
+      <S.SidebarItem iscliicked={item === '뉴스'} onClick={() => HandleCategoryClick('뉴스')}>
+        <img src={item === '뉴스' ? NewsClicked : News} alt="" />
         <span>뉴스</span>
       </S.SidebarItem>
-      <S.SidebarItem iscliicked={item === '뉴스'} onClick={() => setItem('뉴스')}>
+      <S.SidebarItem iscliicked={false}>
         <img src={News} alt="" />
         <span>뉴스 스크랩</span>
       </S.SidebarItem>
@@ -35,7 +35,7 @@ const Sidebar = () => {
           </div>
         ))}
       </S.NewsScrap>
-      <S.SidebarItem iscliicked={item === '뉴스'} onClick={() => setItem('뉴스')}>
+      <S.SidebarItem iscliicked={false}>
         <img src={URL} alt="" />
         <span>URL 저장</span>
       </S.SidebarItem>
@@ -48,9 +48,9 @@ const Sidebar = () => {
             <button
               style={{ width: '30%', fontSize: '20px', background: '#F9D2E7', borderRadius: 14, cursor: 'pointer' }}
               onClick={() =>
-                deleteSavedUrl.mutate(planId, {
+                deleteSavedUrl.mutate(url.id, {
                   onSuccess: () => {
-                    // queryClient.invalidateQueries(QUERY_KEYS.news.savedUrl(planId));
+                    queryClient.invalidateQueries(QUERY_KEYS.news.savedUrl(planId));
                   },
                 })
               }
@@ -61,7 +61,7 @@ const Sidebar = () => {
         ))}
       </S.NewsScrap>
       <S.SidebarItem iscliicked={item === '작성'} onClick={() => HandleCategoryClick('작성')}>
-        <img src={News} alt="" />
+        <img src={item === '작성' ? AIClicked : AI} alt="" />
         <span>작성</span>
       </S.SidebarItem>
       <S.InButton src={InButton} onClick={() => setIsVisible((prev) => !prev)} />
